09bc9df0ece69f5aa5dbb2c5a6bdce9f287f286c
Joshua Mankelow <joshua@mankelow.org>
2023-11-14 23:03:54 +0000

    Made the pattern loading work :)

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod camera;
+mod pattern;
 mod player;
 mod ui;
 mod world;
@@ -7,17 +8,21 @@ use bevy::prelude::*;
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
 use crate::camera::CameraManagerPlugin;
+use crate::pattern::PatternManagerPlugin;
 use crate::player::PlayerManagerPlugin;
 use crate::ui::UiManagerPlugin;
 use crate::world::WorldManagerPlugin;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
-        .add_plugins(CameraManagerPlugin)
-        .add_plugins(PlayerManagerPlugin)
-        .add_plugins(UiManagerPlugin)
-        .add_plugins(WorldManagerPlugin)
-        .add_plugins(WorldInspectorPlugin::default())
+        .add_plugins((
+            DefaultPlugins,
+            CameraManagerPlugin,
+            PatternManagerPlugin,
+            PlayerManagerPlugin,
+            UiManagerPlugin,
+            WorldManagerPlugin,
+            WorldInspectorPlugin::default(),
+        ))
         .run();
 }
--- src/pattern/mod.rs
+++ src/pattern/mod.rs
@@ -10,13 +10,28 @@ pub struct PatternManagerPlugin;
 impl Plugin for PatternManagerPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins(TomlAssetPlugin::<Pattern>::new(&["pattern.toml"]))
-            .add_systems(Startup, (load_patterns, validate_patterns).chain());
+            .add_systems(Startup, load_patterns.chain())
+            .add_systems(Update, validate_patterns);
     }
 }
 
 struct PatternStore {
     size: i32,
     hashmap: HashMap<String, Pattern>,
+}
+
+// #[derive(Resource)]
+// struct PatternHandle(Handle<Pattern>);
+
+#[derive(Resource)]
+struct PatternHandles(Vec<HandleUntyped>);
+
+#[derive(serde::Deserialize, TypeUuid, TypePath, Debug)]
+#[uuid = "413be529-bfeb-41b3-9db0-4b8b380a2c46"]
+struct Pattern {
+    metadata: PatternMetadata,
+    #[serde(alias = "pattern_data")]
+    data: PatternData,
 }
 
 #[derive(serde::Deserialize, Debug)]
@@ -68,34 +83,27 @@ enum ObjectType {
     PowerupSpawnHigh,
 }
 
-#[derive(serde::Deserialize, TypeUuid, TypePath, Debug)]
-#[uuid = "413be529-bfeb-41b3-9db0-4b8b380a2c46"]
-struct Pattern {
-    metadata: PatternMetadata,
-    #[serde(alias = "pattern_data")]
-    data: PatternData,
-}
-
 fn load_patterns(mut commands: Commands, server: Res<AssetServer>) {
     // collect all patterns from filesystem and parse them into
     // Rust objects.
 
     info!("Loading patterns...");
-    let handles = server.load_folder("patterns");
-    match handles {
-        Ok(_) => {}
-        Err(e) => panic!("{}", e),
+    // let pattern = PatternHandle(server.load("patterns/one.pattern.toml"));
+    // commands.insert_resource(pattern);
+    if let Ok(handles) = server.load_folder("patterns") {
+        commands.insert_resource(PatternHandles(handles));
     }
 }
 
-fn validate_patterns(patterns: Res<Assets<Pattern>>) {
+fn validate_patterns(mut pattern_assets: Res<Assets<Pattern>>) {
     // validate patterns (i.e. ensure nothing happens between a
     // CoinArcStart and a CoinArcStop or that they're not too
     // far apart)
 
-    info!("Validating patterns...");
-    for pattern in patterns.iter() {
-        println!("pattern: {:?}", pattern);
+    info!("{}", pattern_assets.len());
+
+    for pattern_asset in pattern_assets.iter() {
+        info!("{:?}", pattern_asset.1.metadata);
     }
 
     // check all patterns are compatible (i.e. no dead ends)
